7e096ff70ace392f5692fa7a85f5329950047b36
THE_GODFATHERR_X <mm1miq.d@gmail.com>
2021-05-19 15:20:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,9 @@ do
         ts-node index.ts "$1"
         echo "Sleeping for $(cat time.txt) seconds"
         sleep "$(cat time.txt)"
+        getcat="$(cat time.txt)"
+        newtime=$(((getcat+2)*2))
+        echo "$newtime" > time.txt
+        echo "time.txt: $newtime"
     fi
 done
